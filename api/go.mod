89e78e7290faec002d7611fe1907c985a0278650
Raein Soltani <raeen.soltani@yahoo.com>
2024-07-05 11:28:39 +0330

    chore: Update Kubernetes deployment and health-check-cronjob configurations

--- api/go.mod
+++ api/go.mod
@@ -10,6 +10,8 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
@@ -25,6 +27,7 @@ require (
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/labstack/echo-contrib v0.17.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -32,13 +35,17 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_golang v1.19.1 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.53.0 // indirect
+	github.com/prometheus/procfs v0.13.0 // indirect
 	github.com/sethvargo/go-password v0.3.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
-	golang.org/x/oauth2 v0.10.0 // indirect
+	golang.org/x/oauth2 v0.18.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/term v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- api/main.go
+++ api/main.go
@@ -1,54 +1,107 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"path/filepath"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sethvargo/go-password/password"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
+	"k8s.io/client-go/rest"
 )
 
+var (
+	totalRequests = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Number of get requests.",
+		},
+		[]string{"path"},
+	)
+	failedRequests = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_failed_total",
+			Help: "Number of failed get requests.",
+		},
+		[]string{"path", "error"},
+	)
+	requestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Duration of HTTP requests.",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"path"},
+	)
+)
+
+func init() {
+	// Register metrics with Prometheus's default registry.
+	prometheus.MustRegister(totalRequests)
+	prometheus.MustRegister(failedRequests)
+	prometheus.MustRegister(requestDuration)
+}
+
+func requestMetricsMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		path := c.Path()
+
+		start := time.Now()
+		totalRequests.WithLabelValues(path).Inc()
+
+		if err := next(c); err != nil {
+			c.Error(err)
+			failedRequests.WithLabelValues(path, err.Error()).Inc()
+			return err
+		}
+
+		duration := time.Since(start).Seconds()
+		requestDuration.WithLabelValues(path).Observe(duration)
+
+		return nil
+	}
+}
+
 func main() {
-	// Use external access config
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
+	// // Use external access config
+	// var kubeconfig *string
+	// if home := homedir.HomeDir(); home != "" {
+	// 	kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	// } else {
+	// 	kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	// }
+	// flag.Parse()
 
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err)
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err)
-	}
-
-	// // Use in-cluster config
-	// config, err := rest.InClusterConfig()
+	// config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	// if err != nil {
-	// 	panic(err.Error())
+	// 	panic(err)
 	// }
 	// clientset, err := kubernetes.NewForConfig(config)
 	// if err != nil {
-	// 	panic(err.Error())
+	// 	panic(err)
 	// }
+
+	// Use in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
 
 	// setup an echo server
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(requestMetricsMiddleware)
 
 	e.GET("/deployments/:appName", func(c echo.Context) error {
 		appName := c.Param("appName")
@@ -129,6 +182,28 @@ func main() {
 			return c.String(http.StatusNotFound, fmt.Sprintf("App type not found: %v", appType))
 		}
 	})
+
+	e.GET("/healthz", func(c echo.Context) error {
+		return c.NoContent(http.StatusOK)
+	})
+
+	e.GET("/readiness", func(c echo.Context) error {
+		_, err := getAllDeploymentsInfo(clientset)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, fmt.Sprintf("Readiness check failed: %v", err))
+		}
+		return c.NoContent(http.StatusOK)
+	})
+
+	e.GET("/startup", func(c echo.Context) error {
+		_, err := getAllDeploymentsInfo(clientset)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, fmt.Sprintf("Startup check failed: %v", err))
+		}
+		return c.NoContent(http.StatusOK)
+	})
+
+	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
 
 	e.Logger.Fatal(e.Start(":8081"))
 }
